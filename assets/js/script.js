f2104caaf9aa881126c30485666dea9488230eaf
Kvz666 <55993619+Kvz666@users.noreply.github.com>
2021-10-08 02:14:52 +0200

    I finished responsive only for IphoneeX models

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,85 +1,88 @@
-const avatarGit = document.querySelector(".avatar")
-const nameGit = document.querySelector(".name")
-const date = document.querySelector(".date")
-const usernameGit = document.querySelector(".username")
-const biography = document.querySelector(".biography")
-const repository = document.querySelector(".repository")
-const follower = document.querySelector(".follower")
-const following = document.querySelector(".following")
-const city = document.querySelector(".city")
-const blog = document.querySelector(".blog")
-const twitter = document.querySelector(".twitter")
-const company = document.querySelector(".company")
-const body = document.querySelector("body")
-const button = document.querySelector(".search-btn")
-const input = document.querySelector(".search-input")
-const colorLight = document.querySelector(".color-light")
+const avatarGit = document.querySelector(".avatar");
+const nameGit = document.querySelector(".name");
+const date = document.querySelector(".date");
+const usernameGit = document.querySelector(".username");
+const biography = document.querySelector(".biography");
+const repository = document.querySelector(".repository");
+const follower = document.querySelector(".follower");
+const following = document.querySelector(".following");
+const city = document.querySelector(".city");
+const blog = document.querySelector(".blog");
+const twitter = document.querySelector(".twitter");
+const company = document.querySelector(".company");
+const body = document.querySelector("body");
+const button = document.querySelector(".search-btn");
+const input = document.querySelector(".search-input");
+const colorLight = document.querySelector(".color-light");
+
+
+
 
 const loadUser = (username) => {
     fetch(`https://api.github.com/users/${username}`).then((response)=>{
         if(response.ok)
         {
-            return response.json()
+            return response.json();
         }
         else
         {
-            document.querySelector(".no-result").classList.add("visible")
+            document.querySelector(".no-result").classList.add("visible");
         }
     }
     ).then((data)=>{
-        console.log(data)
-        const dateGit = new Date(data.created_at)
-        avatarGit.querySelector("img").src = data.avatar_url
-        nameGit.innerHTML = data.name === "" || data.name === null ? data.login : data.name
-        date.innerHTML = dateGit.getDate()+" "+dateGit.toLocaleString("default", { month: "short" })+" "+dateGit.getFullYear()
-        usernameGit.innerHTML = "@" + data.login
-        biography.innerHTML = data.bio === "" || data.bio === null ? "This User has no bio" : data.bio
-        repository.innerHTML = data.public_repos
-        follower.innerHTML = data.followers
-        following.innerHTML = data.following
-        city.innerHTML = data.location === "" || data.location === null ? "Not Available" : data.location
-        blog.innerHTML = data.blog === "" || data.blog === null ? "Not Available" : data.blog
-        twitter.innerHTML = data.twitter_username === "" || data.twitter_username === null ? "Not Available" : data.twitter_username
-        company.innerHTML = data.company === "" || data.company === null ? "Not Available" : data.company
+        console.log(data);
+        const dateGit = new Date(data.created_at);
+        avatarGit.querySelector("img").src = data.avatar_url;
+        nameGit.innerHTML = data.name === "" || data.name === null ? data.login : data.name;
+        date.innerHTML = dateGit.getDate()+" "+dateGit.toLocaleString("default", { month: "short" })+" "+dateGit.getFullYear();
+        usernameGit.innerHTML = "@" + data.login;
+        biography.innerHTML = data.bio === "" || data.bio === null ? "This User has no bio" : data.bio;
+        repository.innerHTML = data.public_repos;
+        follower.innerHTML = data.followers;
+        following.innerHTML = data.following;
+        city.innerHTML = data.location === "" || data.location === null ? "Not Available" : data.location;
+        blog.innerHTML = data.blog === "" || data.blog === null ? "Not Available" : data.blog;
+        twitter.innerHTML = data.twitter_username === "" || data.twitter_username === null ? "Not Available" : data.twitter_username;
+        company.innerHTML = data.company === "" || data.company === null ? "Not Available" : data.company;
     }).catch((error)=>{
-        console.log(error)
+        console.log(error);
     })
 }
 
 window.addEventListener("load",(e)=>{
-    loadUser("octocat")
+    loadUser("octocat");
     if(localStorage.getItem('theme')==='light')
     {
-        document.documentElement.classList.add("light")
-        document.documentElement.classList.remove("dark")
+        document.documentElement.classList.add("light");
+        document.documentElement.classList.remove("dark");
     }
     else if(localStorage.getItem('theme')==='dark')
     {
-        document.documentElement.classList.add("dark")
-        document.documentElement.classList.remove("light")
+        document.documentElement.classList.add("dark");
+        document.documentElement.classList.remove("light");
     }
     else
     {
         if(window.matchMedia('(prefers-color-scheme: dark)'))
         {
-            document.documentElement.classList.add("dark")
-            localStorage.setItem('theme','dark')
+            document.documentElement.classList.add("dark");
+            localStorage.setItem('theme','dark');
         }
         else
         {
-            document.documentElement.classList.add("light")
-            localStorage.setItem('theme','light')
+            document.documentElement.classList.add("light");
+            localStorage.setItem('theme','light');
         }
     }
-    console.log(localStorage)
+    console.log(localStorage);
 })
 button.addEventListener("click",function(){
-    loadUser(input.value)
+    loadUser(input.value);
 })
 input.addEventListener("keypress",function(e){
     if(e.key==="Enter")
     {
-        loadUser(input.value)
+        loadUser(input.value);
     }
 })
 
@@ -87,28 +90,29 @@ input.addEventListener("keypress",function(e){
 colorLight.addEventListener("click",function(){
     if(localStorage.getItem('theme')==='light')
     {
-        document.documentElement.classList.remove("light")
-        document.documentElement.classList.add("dark")
-        localStorage.setItem('theme','dark')
+        document.documentElement.classList.remove("light");
+        document.documentElement.classList.add("dark");
+        localStorage.setItem('theme','dark');
     }
     else if(localStorage.getItem('theme')==='dark')
     {
-        document.documentElement.classList.remove("dark")
-        document.documentElement.classList.add("light")
-        localStorage.setItem('theme','light')
+        document.documentElement.classList.remove("dark");
+        document.documentElement.classList.add("light");
+        localStorage.setItem('theme','light');
     }
     else
     {
         if(window.matchMedia('(prefers-color-scheme: dark)'))
         {
-            document.documentElement.classList.add("dark")
-            localStorage.setItem('theme','dark')
+            document.documentElement.classList.add("dark");
+            localStorage.setItem('theme','dark');
         }
         else
         {
-            document.documentElement.classList.add("light")
-            localStorage.setItem('theme','light')
+            document.documentElement.classList.add("light");
+            localStorage.setItem('theme','light');
         }
     }
-    console.log(localStorage)
+    console.log(localStorage);
 })
+
